eaca2f5d829905b173fcb172bf3be2cc85429ef1
AndIAmSam <samflogar3@gmail.com>
2024-03-18 13:07:44 -0600

    Backend. V1.

---
+++ backend/routes/tasks.routes.js
@@ -0,0 +1,17 @@
+import {Router} from 'express';
+import { getTasks, getTask, createTasks, updateTask, deleteTask } from '../controlers/task.controllers.js';
+
+const router = Router();
+
+router.get('/tasks', getTasks)
+
+router.get('/tasks/:id', getTask)
+
+router.post('/tasks', createTasks)
+
+router.put('/tasks/:id', updateTask)
+
+router.delete('/tasks/:id', deleteTask)
+
+
+export default router;
