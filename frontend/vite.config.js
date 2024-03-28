5818210ce2ab38e052583e971e1a4d6239f3d683
CarlosGD17 <carlosgd_17@outlook.com>
2024-03-27 23:02:16 -0600

    parche para ip:port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Set your custom IP address here
+    port: 5137, // Set your custom port number here
+  },
 })
