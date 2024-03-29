dc49443577fa23d97d2b15f5cfa0c03ec8678426
CarlosGD17 <carlosgd_17@outlook.com>
2024-03-28 22:00:21 -0600

    browser redirection solved

--- frontend/src/api/tasks.api.js
+++ frontend/src/api/tasks.api.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
 export const getTasksRequest = async () => 
-  await axios.get("http://back:3000/tasks");
+  await axios.get("http://localhost:3000/tasks");
 
 export const createTaskRequest = async (task) => 
-  await axios.post("http://back:3000/tasks", task);
+  await axios.post("http://localhost:3000/tasks", task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://back:3000/tasks/${id}`);
+  await axios.delete(`http://localhost:3000/tasks/${id}`);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(`http://back:3000/task/${id}`);
+  await axios.get(`http://localhost:3000/task/${id}`);
 
   export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`http://back:3000/tasks/${id}`, newFields);
+  await axios.put(`http://localhost:3000/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) => {
   console.warn(done, "si llega")
-  await axios.put(`http://back:3000/tasks/${id}`, done);
+  await axios.put(`http://localhost:3000/tasks/${id}`, done);
 }
 
 export const searchTasksRequest = async (search) =>
-  await axios.get(`http://back:3000/tasks/search?query=${search}`);
+  await axios.get(`http://localhost:3000/tasks/search?query=${search}`);
 
 
