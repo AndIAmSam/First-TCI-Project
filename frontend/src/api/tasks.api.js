2a4295f857b6f280a720075e2793ccd1badd7803
CarlosGD17 <carlosgd_17@outlook.com>
2024-03-31 16:35:14 -0600

    ip reference workaround

--- frontend/src/api/tasks.api.js
+++ frontend/src/api/tasks.api.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
 export const getTasksRequest = async () => 
-  await axios.get("http://localhost:3000/tasks");
+  await axios.get(`${process.env.REACT_APP_BACKEND_URL}/tasks`);
 
 export const createTaskRequest = async (task) => 
-  await axios.post("http://localhost:3000/tasks", task);
+  await axios.post(`${process.env.REACT_APP_BACKEND_URL}/tasks`, task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://localhost:3000/tasks/${id}`);
+  await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/tasks/${id}`);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(`http://localhost:3000/task/${id}`);
+  await axios.get(`${process.env.REACT_APP_BACKEND_URL}/${id}`);
 
   export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:3000/tasks/${id}`, newFields);
+  await axios.put(`${process.env.REACT_APP_BACKEND_URL}/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) => {
   console.warn(done, "si llega")
-  await axios.put(`http://localhost:3000/tasks/${id}`, done);
+  await axios.put(`${process.env.REACT_APP_BACKEND_URL}/tasks/${id}`, done);
 }
 
 export const searchTasksRequest = async (search) =>
-  await axios.get(`http://localhost:3000/tasks/search?query=${search}`);
+  await axios.get(`${process.env.REACT_APP_BACKEND_URL}/tasks/search?query=${search}`);
 
 
